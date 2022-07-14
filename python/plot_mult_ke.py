86f367eb43ed269d0224505a9fc663c877a0af5b
J. S. Oishi <jsoishi@gmail.com>
2022-07-14 09:35:59 -0700

    Updated plot_mult_ke.py as in README.md

--- python/plot_mult_ke.py
+++ python/plot_mult_ke.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 
-curtis = np.loadtxt("fig_kinetic_t_eps0.4.dat")
+runs = {"DNS":"runs/eps_0.4_run_GQL_test", "L0":"runs/eps_0.4_run_GQL_L0", "L1":"runs/eps_0.4_run_GQL_L1", "L5":"runs/eps_0.4_run_GQL_L5"}
+curtis = {"DNS":"fig_kinetic_t_eps0.4.dat", "L0":"fig_kinetic_t_eps0.4_L0.dat", "L1":"fig_kinetic_t_eps0.4_L1.dat", "L5":"fig_kinetic_t_eps0.4_L5.dat"}
 
 Lx = 25
 Ly = 25
@@ -11,25 +12,32 @@ t = {}
 KE = {}
 Re = {}
 Nu = {}
-
-runs = ("128x128x72", "128x128x32", "64x64x16")
-dirs = ("eps_0.4_run_128_128_72","eps_0.4_run_128_128_32","eps_0.4_run")
-
-for name, rdir in zip(runs, dirs):
-    
-    ts = h5py.File(rdir + "/scalar/scalar.h5","r")
-    t[name]  = ts['scales/sim_time'][:]
-    KE[name] = ts['tasks/KE'][:,0,0,0]/(Lx*Ly*Lz)
-    Re[name] = ts['tasks/Re_rms'][:,0,0,0]
-    Nu[name] = ts['tasks/Nusselt'][:,0,0,0]
+tc = {}
+KEc = {}
+for k, v in runs.items():
+    ts = h5py.File(v + "/scalar/scalar_s1/scalar_s1_p0.h5","r")
+    t [k]  = ts['scales/sim_time'][:]
+    KE[k] = ts['tasks/KE'][:,0,0,0]/(Lx*Ly*Lz)
+    Re[k] = ts['tasks/Re_rms'][:,0,0,0]
+    Nu[k] = ts['tasks/Nusselt'][:,0,0,0]
     ts.close()
+    c = np.loadtxt(curtis[k])
+    tc[k] = c[:,0]
+    KEc[k] = c[:,1]
 
 plt.figure()
 for k,v in t.items():
-    plt.plot(t[k],KE[k], label=k)
-plt.plot(curtis[:,0], curtis[:,1], label='Curtis')
+    plt.plot(t[k],KE[k], 'x', label=k)
+
+plt.gca().set_prop_cycle(None)
+for k, v in tc.items():
+    plt.plot(tc[k], KEc[k])
 plt.xlabel(r'$t/t_{\kappa}$')
 plt.ylabel('KE')
-#plt.xlim(0,400)
+plt.xlim(0,150)
+plt.ylim(13,13.6)
 plt.legend()
-plt.savefig("KE_vs_t.png",dpi=300)
+plt.savefig("mult_KE_vs_t.png",dpi=300)
+
+
+
